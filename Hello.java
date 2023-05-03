98a959b70c72320edfdc196ac9df9dbfc1c0b13b
rajkashy <rajkashy889@gmail.com>
2023-05-03 19:49:20 +0530

    Driver java class created

--- Hello.java
+++ Hello.java
@@ -2,6 +2,9 @@
  * Hello
  */
 public class Hello {
+    void show(){
+        System.out.println("show msg");
+    }
 public static void main(String[] args) {
     System.out.println("Hello Java");
 }
